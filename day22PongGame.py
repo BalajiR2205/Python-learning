9b011bb03259eb6257eff139421bb041f2afcdbc
BalajiR2205 <Balaji.Rajarathnam@thomsonreuters.com>
2025-07-13 15:43:09 +0530

    Functions and kernals - Day 8

---
+++ day22PongGame.py
@@ -0,0 +1,25 @@
+#import time
+from turtle import Screen
+
+screen = Screen()
+screen.setup(800, 600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+screen.tracer(0)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
